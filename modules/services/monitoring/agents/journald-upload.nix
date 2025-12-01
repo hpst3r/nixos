80b3490cd0c5a08d5a4298eda2f45e9b43a7d40a
William Porter <liam@wporter.org>
2025-11-30 19:20:40 -0500

    toggle zstd compression on journald.upload and increase max post size for nginx in front of victorialogs

--- modules/services/monitoring/agents/journald-upload.nix
+++ modules/services/monitoring/agents/journald-upload.nix
@@ -5,9 +5,11 @@
     enable = true;
     settings.Upload = {
       URL = "https://victorialogs.lab.wporter.org:443/insert/journald";
+      Compression = "zstd:3";
       ServerCertificateFile = "-"; # RTFM - this, not null, disables client cert auth
       ServerKeyFile = "-";
       TrustedCertificateFile = "-"; # don't use /etc/ssl/ca/trusted.pem
-    };
+    }; # for a full list of options, see journal-upload.conf docs:
+    # https://www.freedesktop.org/software/systemd/man/latest/journal-upload.conf.html#
   };
 }
--- modules/services/monitoring/servers/victorialogs/nginx-victorialogs.nix
+++ modules/services/monitoring/servers/victorialogs/nginx-victorialogs.nix
@@ -25,8 +25,11 @@
       locations."/insert/journald" = {
         proxyPass = "http://localhost:9428";
         extraConfig = ''
-	        client_max_body_size 256M;
-        ''; # journal uploads can be quite large & journal-upload.service will just kill itself if it can't shove a whole load into the server at once
+	        client_max_body_size 5G;
+        ''; # journal uploads can be quite large & journal-upload.service will just kill itself
+        # if it can't send a whole load of logs at the server at once. there is no way to limit this
+        # on the client side AFAIK. as a workaround, toggle compression on client & allow big POSTs.
+        # https://www.freedesktop.org/software/systemd/man/latest/journal-upload.conf.html
       };
     };
   };
