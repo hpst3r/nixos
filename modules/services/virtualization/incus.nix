0576c6350342a0b0d2e663d2b04d73cbf25386ef
William Porter <liam@wporter.org>
2025-12-28 22:09:17 -0500

    fix incus preseed config

--- modules/services/virtualization/incus.nix
+++ modules/services/virtualization/incus.nix
@@ -1,29 +1,25 @@
-{}: {
+{...}: {
   networking.nftables.enable = true;
-  virtualization.incus = {
+  virtualisation.incus = {
     enable = true;
     preseed = {
-      networks = [ {
-          name = "incusbr0";
-          type = "bridge";
-          config = {
-            ipv4.address = "192.0.2.0/24";
-            ipv4.nat = "true";
-            ipv6.address = "none";
+      "networks" = [ {
+          "name" = "incusbr0";
+          "type" = "bridge";
+          "config" = {
+            "ipv4.address" = "192.0.2.1/24";
+            "ipv4.nat" = "true";
           };
         }
       ];
-      storage_pools = [ {
-          config = {
-            source = "/var/lib/incus/storage-pools/default";
+      "storage_pools" = [ {
+          "config" = {
+            "source" = "/var/lib/incus/storage-pools/default";
           };
-          driver = "dir";
-          name = "default";
+          "driver" = "dir";
+          "name" = "default";
         }
       ];
-    }
-  };
-  services.incus = {
-    enable = true;
+    };
   };
 }
