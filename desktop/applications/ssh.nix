cf2a7166b39cd2077583b00fcdb5f07422cc1af1
William Porter <liam@wporter.org>
2026-02-01 18:14:09 -0500

    adjust sx6036 ip

--- desktop/applications/ssh.nix
+++ desktop/applications/ssh.nix
@@ -7,8 +7,12 @@ in {
     enable = true;
     enableDefaultConfig = false;
     matchBlocks."*".identityAgent = onePassSSHAgent;
-    # mellanox sx6036
-    matchBlocks."192.168.77.200".extraOptions = {
+    # mellanox sx6036 - temp ip
+    matchBlocks."172.27.254.250".extraOptions = {
+      KexAlgorithms = "+diffie-hellman-group14-sha1";
+      HostKeyAlgorithms = "+ssh-rsa,ssh-rsa-cert-v01@openssh.com";
+    };
+    matchBlocks."sx6036a.lab.wporter.org".extraOptions = {
       KexAlgorithms = "+diffie-hellman-group14-sha1";
       HostKeyAlgorithms = "+ssh-rsa,ssh-rsa-cert-v01@openssh.com";
     };
